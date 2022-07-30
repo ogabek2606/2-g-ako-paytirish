88264ec311ddc2557bed84d4c69f678b5d0c6aab
ogabek2606 <110012533+ogabek2606@users.noreply.github.com>
2022-07-30 03:42:18 -0400

    Add files via upload

---
+++ untitled22/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int n = 8;
+
+        if (n > 10) {
+            System.out.println(n + 3);
+        } else if (n < 10) {
+            System.out.println(n * 2);
+        } else {
+            System.out.println(22);
+        }
+
+    }
+}
